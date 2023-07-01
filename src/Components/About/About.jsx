1a2cc9e607d7148d2c3e4893b2a5e2dfb922a769
Margub Murshed <murshedmargub@gmail.com>
2023-07-01 13:55:30 +0600

    about section  done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import photo from "../../assets/myphoto.png";
 
 const About = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section className="p-5 py-10 bg-gray-50" id="about">
+      <div className="container mx-auto lg:flex gap-5 lg:w-3/4">
+        <img src={photo} alt="margub murshed photo" className="max-w-[200px] mx-auto border-primary border-2 rounded-md"/>
+        <div className="space-y-2">
+          <h2 className="font-source-pro text-3xl font-semibold uppercase text-slate-900 mt-5 text-center lg:mt-0 lg:text-left">About Me</h2>
+          <p className="text-gray-700 font-light">
+            Hi, I'm Margub Murshed, a full stack web developer. Web design &
+            development is my passion. I love to explore new tech things. I'm
+            very passionate and dedicated to my work. I have expertise in
+            Front-End and Back-End Web development too. I can design a website
+            fully responsive & dynamic as per requirment. I enjoy every step of
+            the design & development process from discussion and collaboration.
+            I have experience in React JS, Next JS, Node JS, MongoDB, Experess,
+            Tailwind CSS, Material UI, Bootstrap etc.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import illustration from '../../assets/hero-illustration.svg';
 const Hero = () => {
     return (
         <section className='text-center space-y-5 p-5 py-10' id='home'>
-            <h1 className='font-source-pro text-4xl font-bold'>HI, I'M MARGUB. I'M A FRONT-END DEVELOPER</h1>
+            <h1 className='font-source-pro text-4xl font-bold text-slate-900'>HI, I'M MARGUB. I'M A FRONT-END DEVELOPER</h1>
             <p className=' font-extralight text-xl'>I love to make things clean And attractive. Love to learn everyday</p>
             <a href='resume.pdf' download className='inline-block border-primary border-2 px-5 py-3 rounded-md bg-slate-900 bg-opacity-90 text-white font-semibold font-source-pro uppercase'>Download Resume</a>
             <img src={illustration} alt="hero-illustration" className='w-full max-w-lg mx-auto'/>
