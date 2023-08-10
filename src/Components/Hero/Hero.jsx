6af14eb6812df6485ff8f359cb41d40c0a4a98cc
Margub Murshed <83542971+margubmurshed@users.noreply.github.com>
2023-08-11 02:01:27 +0600

    Updated title and download link in Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,13 +6,13 @@ const Hero = () => {
     return (
         <section className='text-center space-y-5 p-5 py-10' id='home'>
             <Slide triggerOnce direction='left' duration={400}>
-            <h1 className='font-source-pro text-4xl font-bold text-slate-900'>HI, I'M MARGUB. I'M A FRONT-END DEVELOPER</h1>
+            <h1 className='font-source-pro text-4xl font-bold text-slate-900'>HI, I'M MARGUB. I'M A MERN STACK DEVELOPER</h1>
             </Slide>
             <Slide triggerOnce direction='left' duration={600}>
             <p className=' font-extralight text-xl'>I love to make things clean And attractive. Love to learn everyday</p>
             </Slide>
             <Slide triggerOnce direction='left' duration={800}>
-            <a href='https://drive.google.com/file/d/1KwzKuAEvSveWLqHC84TB-2lKR-zh1fAv/view?usp=sharing' className='inline-block border-primary border-2 px-5 py-3 rounded-md bg-slate-900 bg-opacity-90 text-white font-semibold font-source-pro uppercase'>Download Resume</a>
+            <a href='https://drive.google.com/file/d/1bOc0FWUYgpEx7RNrT_Vg5YEdsYF5VXHY/view?usp=sharing' className='inline-block border-primary border-2 px-5 py-3 rounded-md bg-slate-900 bg-opacity-90 text-white font-semibold font-source-pro uppercase'>Download Resume</a>
             </Slide>
             <Slide triggerOnce direction='left' duration={1000}>
             <img src={illustration} alt="hero-illustration" className='w-full max-w-lg mx-auto'/>
