17f88e415b2ed75838fe738fbd23802bcc817965
Margub Murshed <murshedmargub@gmail.com>
2023-08-11 02:38:58 +0600

    Contact form styles updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -10,12 +10,12 @@ import emailjs from "@emailjs/browser";
 import Swal from "sweetalert2";
 
 const Contact = () => {
-    const [disabled, setDisabled] = useState(false);
+  const [disabled, setDisabled] = useState(false);
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-    setDisabled(true)
+    setDisabled(true);
 
     emailjs
       .sendForm(
@@ -29,11 +29,11 @@ const Contact = () => {
           console.log(result.text);
           Swal.fire("Success!", "Message sent successfully", "success");
           e.target.reset();
-          setDisabled(false)
+          setDisabled(false);
         },
         (error) => {
           Swal.fire("Failed!", error.text, "error");
-          setDisabled(false)
+          setDisabled(false);
         }
       );
   };
@@ -49,41 +49,59 @@ const Contact = () => {
           </p>
         </div>
         <div>
-          <form onSubmit={sendEmail} ref={form} className="max-w-lg mx-auto border border-primary mt-5 p-5 rounded-md space-y-3">
-            <div className="flex flex-col">
-              <label className="text-white text-sm mb-1" htmlFor="name">Your name</label>
-              <input
-                type="text"
-                id="name"
-                placeholder="Enter your name"
-                name="from_name"
-                className="px-5 py-3 outline-none rounded-md"
-                required
-              />
+          <form
+            onSubmit={sendEmail}
+            ref={form}
+            className="max-w-3xl mx-auto border border-primary mt-5 p-5 rounded-md space-y-3"
+          >
+            <div className="flex flex-col md:flex-row gap-3">
+              <div className="flex-flex col md:w-1/2">
+                <label className="text-white text-sm mb-1" htmlFor="name">
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Enter your name"
+                  name="from_name"
+                  className="px-5 py-3 outline-none rounded-md w-full focus:bg-primary focus:text-white placeholder:focus:text-white"
+                  required
+                />
+              </div>
+              <div className="flex flex-col md:w-1/2">
+                <label className="text-white text-sm mb-1" htmlFor="email">
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  name="from_email"
+                  className="px-5 py-3 outline-none rounded-md focus:bg-primary focus:text-white placeholder:focus:text-white"
+                  required
+                />
+              </div>
             </div>
             <div className="flex flex-col">
-              <label className="text-white text-sm mb-1" htmlFor="email">Your email</label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
-                name="from_email"
-                className="px-5 py-3 outline-none rounded-md"
-                required
-              />
-            </div>
-            <div className="flex flex-col">
-              <label className="text-white text-sm mb-1" htmlFor="message">Your message</label>
+              <label className="text-white text-sm mb-1" htmlFor="message">
+                Your message
+              </label>
               <textarea
                 placeholder="Enter your message"
                 id="message"
                 name="message"
-                className="px-5 py-3 outline-none rounded-md"
+                className="px-5 py-3 outline-none rounded-md focus:bg-primary focus:text-white placeholder:focus:text-white"
                 rows={10}
                 required
               ></textarea>
             </div>
-            <button type="submit" disabled={disabled} className='inline-block border-primary border-2 px-5 py-3 rounded-md bg-slate-900 bg-opacity-90 text-white font-semibold font-source-pro uppercase w-full'>Send Message</button>
+            <button
+              type="submit"
+              disabled={disabled}
+              className="inline-block border-primary border-2 px-5 py-3 rounded-md bg-slate-900 bg-opacity-90 text-white font-semibold font-source-pro uppercase w-full"
+            >
+              Send Message
+            </button>
           </form>
         </div>
         <div className="flex flex-col items-center mt-5">
@@ -94,25 +112,41 @@ const Contact = () => {
             <MdLocationPin size={30} /> <span>Dhaka, Bangladesh</span>
           </p>
           <div className="flex justify-center items-center gap-3 mt-5">
-            <a href="https://www.linkedin.com/in/margubmurshed/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.linkedin.com/in/margubmurshed/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BiLogoLinkedin
                 size={40}
                 className="p-2 bg-white text-gray-800 rounded-full shadow-md"
               />
             </a>
-            <a href="https://github.com/margubmurshed" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/margubmurshed"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BiLogoGithub
                 size={40}
                 className="p-2 bg-white text-gray-800 rounded-full shadow-md"
               />
             </a>
-            <a href="facebook.com/margub32" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://www.facebook.com/margub32"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BiLogoFacebook
                 size={40}
                 className="p-2 bg-white text-gray-800 rounded-full shadow-md"
               />
             </a>
-            <a href="https://m.me/margub32/" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://m.me/margub32/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <BiLogoMessenger
                 size={40}
                 className="p-2 bg-white text-gray-800 rounded-full shadow-md"
